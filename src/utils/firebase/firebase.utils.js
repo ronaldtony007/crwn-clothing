e3ac1703d6e5a8e4dc0b9daaf21e7af24b5bad7f
ronaldtony007 <ronaldt41@outlook.com>
2022-08-17 21:46:54 +0530

    Setup authentication

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,52 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAgxOO3_2-HHZB5B61ELpqsfrq9dAt5HlA",
+  authDomain: "crwn-clothing-react-course-db.firebaseapp.com",
+  projectId: "crwn-clothing-react-course-db",
+  storageBucket: "crwn-clothing-react-course-db.appspot.com",
+  messagingSenderId: "315214933247",
+  appId: "1:315214933247:web:a5e6e7056b0336660870ca",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("error creating the user", error.message);
+    }
+  }
+
+  return userDocRef;
+};
