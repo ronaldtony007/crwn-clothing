fe1b9f8e6eb3f98bced3395a56fce9f78e087859
ronaldtony007 <ronaldt41@outlook.com>
2022-09-04 18:41:33 +0530

    Updated styled components for routes

---
+++ src/routes/authentication/authentication.styles.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const AuthenticationContainer = styled.div`
+  display: flex;
+  width: 900px;
+  justify-content: space-around;
+  margin: 30px auto;
+`;
